8338b8e05f43a4bb38aacba2f7f1c1445350ae54
Lucas Anjos <oargus.g@gmail.com>
2023-11-11 17:10:30 -0300

    Add __str__ methods and foi_publicado_recentemente function to models.py

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,14 +1,27 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Questao(models.Model):
+
+    def __str__(self):
+        return self.texto_questao
+
     # Cria um campo de texto com no máximo 200 caracteres.
     texto_questao = models.CharField(max_length=200)
 
     # Cria um campo de data e hora.
     data_publicacao = models.DateTimeField('data de publicação')
 
+    def foi_publicado_recentemente(self):
+        return self.data_publicacao >= timezone.now() - datetime.timedelta(days=1)
+    
 class Escolha(models.Model):
+
+    def __str__(self):
+        return self.texto_escolha
+    
     # Cria uma chave estrangeira para a classe Questao. 
     # Quando a classe Questao for excluída, todas as escolhas relacionadas 
     # a ela também serão excluídas.
